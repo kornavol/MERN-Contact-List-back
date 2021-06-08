806758075f2e74372e9131e261abd9eedf6fc307
kornavol <volosh.andrey@gmail.com>
2021-06-08 15:17:16 +0200

    upload multuple files

--- controller/public.js
+++ controller/public.js
@@ -1,7 +1,20 @@
 const nodemailer = require('nodemailer')
+const multer  = require('multer')
+const path = require('path');
+
 
 const contacts = require('../model/contacts');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'attachments/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'a' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+  const upload = multer({ storage: storage }).array('attachs', 10);
 
 exports.about = (req, res) => {
     contacts.find({}, (err, docs) => {
@@ -14,41 +27,57 @@ exports.about = (req, res) => {
 }
 
 exports.getContact = async (req, res) => {
-    // let transporter = nodemailer.createTransport(transport[, defaults])
+    
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+          // A Multer error occurred when uploading.
+        } else if (err) {
+          // An unknown error occurred when uploading.
+        }
+    })
 
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
+    
+    // // let transporter = nodemailer.createTransport(transport[, defaults])
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com', // for gmail
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: process.env.GMAIL_USER, // generated ethereal user
-            pass: process.env.GMAIL_PASS, // generated ethereal password
-        },
-    });
+    // // Generate test SMTP service account from ethereal.email
+    // // Only needed if you don't have a real mail account for testing
+    // let testAccount = await nodemailer.createTestAccount();
+
+    // // create reusable transporter object using the default SMTP transport
+    // try {
+    //     let transporter = nodemailer.createTransport({
+    //         host: 'smtp.gmail.com', // for gmail
+    //         port: 465,
+    //         secure: true, // true for 465, false for other ports
+    //         auth: {
+    //             user: process.env.GMAIL_USER, // generated ethereal user
+    //             pass: process.env.GMAIL_PASS, // generated ethereal password
+    //         },
+    //     });
 
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Customer Service" <foo@example.com>', // sender address
-        to: "volosh.andrey@gmail.com, ", // list of receivers
-        subject: "Tivket from: " + req.body.fullName, // Subject line
-        text: req.body.message, // plain text body
-        html: `<b>${req.body.message}</b>`, // html body
-    });
+    //     // send mail with defined transport object
+    //     let info = await transporter.sendMail({
+    //         from: '"Customer Service" <foo@example.com>', // sender address
+    //         to: "sinicin.andrii@gmail.com, ", // list of receivers
+    //         subject: "Tivket from: " + req.body.fullName, // Subject line
+    //         text: req.body.message, // plain text body
+    //         html: `<b>${req.body.message}</b>`, // html body
+    //     });
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    //     console.log("Message sent: %s", info.messageId);
+    //     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    //     // Preview only available when sending through an Ethereal account
+    //     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    //     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-    console.log(req.body);
-    /* We also could send json as respond */
-    res.json({ status: "success", message: "Gongrats" });
+    //     console.log(req.body);
+    //     /* We also could send json as respond */
+    //     res.json({ status: "success", message: "Gongrats" });
+    // } catch (err) {
+    //     console.log(err)
+    //     res.status(500).json({ status: "failed", message: err });
+
+    // }
 }
